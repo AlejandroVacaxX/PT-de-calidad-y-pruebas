2230c00922ff71410d0253de95c2c7d5db4d1eae
AlejandroVacaxX <erick.pintor@cua.uam.mx>
2026-04-10 15:32:17 -0600

    merge completo

--- src/main/java/org/alejandro/vaca/persona/controller/PersonaController.java
+++ src/main/java/org/alejandro/vaca/persona/controller/PersonaController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
     
     @RestController
     @RequestMapping("personas/personas-api")
