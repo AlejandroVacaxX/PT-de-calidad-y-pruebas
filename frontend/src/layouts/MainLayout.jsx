444dcd458c5f3112befa203e9428ec2a40d6fdba
Saul Hernandez <saul.hernandez.g@cua.uam.mx>
2026-04-11 20:32:41 -0600

    Otro cambio a MainLayout.jsx para el scroll

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ export const MainLayout = ({ children }) => {
   return (
         <div className="flex h-screen bg-gray-50 font-sans">
           <Sidebar/>
-          <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex flex-col overflow-y-auto">
               {children}
           </div>
         </div>
