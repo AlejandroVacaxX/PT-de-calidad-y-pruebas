c8e55c009d5d7d20be493485cce249d581a0984e
Saul Hernandez <saul.hernandez.g@cua.uam.mx>
2026-04-11 20:14:24 -0600

    Se quitó overflow-hidden

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ export const MainLayout = ({ children }) => {
   return (
         <div className="flex h-screen bg-gray-50 font-sans">
           <Sidebar/>
-          <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="flex-1 flex flex-col">
               {children}
           </div>
         </div>
